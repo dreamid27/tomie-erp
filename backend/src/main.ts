c03a897907947709f1588f7a95907f98676ee7e3
Muhammad Al Faris <muh.alfaris@gmail.com>
2025-07-16 06:10:14 +0700

    chore: initialize backend + swagger

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tomie ERP')
+    .setDescription('The ERP API description')
+    .setVersion('1.0')
+    .addTag('ERP')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
+}
+bootstrap();
