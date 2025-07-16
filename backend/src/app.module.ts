42f3f6487d51a041038cf42a01294f23d6b63115
Muhammad Al Faris <muh.alfaris@gmail.com>
2025-07-16 09:16:29 +0700

    feat: adding quotation and sales-order crud mock

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
+import { QuotationModule } from './quotation/quotation.module';
+import { SalesOrderModule } from './sales-order/sales-order.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), QuotationModule, SalesOrderModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
